9461c5bf13fb9d2b62dc672f2503866b8c68b18c
Shannon Graybrook <sg1578@methode.global>
2024-05-17 05:59:34 -0500

    initial commit

---
+++ ArgSaver/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArgSaver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string logPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\args.txt";
+            using (StreamWriter writer = new StreamWriter(logPath))
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    writer.WriteLine($"Arg[{i}] = [{args[i]}]");
+                }
+                writer.Close();
+            }
+        }
+    }
+}
